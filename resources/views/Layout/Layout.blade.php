6fe8e4e4f2a612cef08e4c93385f36cb9879e28a
EL MEHDI <116589671+elmehdimoubachir@users.noreply.github.com>
2023-08-18 11:45:53 +0100

    edit layout

--- resources/views/Layout/Layout.blade.php
+++ resources/views/Layout/Layout.blade.php
@@ -14,7 +14,7 @@
      <!-- Icon  -->
      <link rel="icon" href="{{ url('assets/Images/iconapp.jpg')}}" type="imge/x-icon" />
 
-     <!-- icons -->
+         <!-- icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
 </head>
@@ -35,8 +35,11 @@
                     <div class="iconh"></div>
                   </div>    
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-5">
                     <span id="navtitle">@yield('title')</span>
+                </div>
+                <div class="col-md-1">
+                <i class="bi bi-bell-fill" style="font-size:25px; float:right;"></i>
                 </div>
                 <div class="col-md-2"> 
                   <form action="/Dashboardsearch" method="GET">
@@ -73,7 +76,7 @@
 
     <section>
         <div class="sidenav" id="hide">
-        <div class="lineMeun d-flex flex-row"><i class="bi bi-grid-1x2-fill"></i><a href="{{ route('Dashboard.index') }}">Dashboard</a></div>
+          <div class="lineMeun d-flex flex-row"><i class="bi bi-grid-1x2-fill"></i><a href="{{ route('Dashboard.index') }}">Dashboard</a></div>
           <div class="lineMeun d-flex flex-row"><i class="bi bi-people-fill"></i><a href="{{ route('Client.index') }}">Clients</a></div>
           <div class="lineMeun d-flex flex-row"><i class="bi bi-car-front-fill"></i><a href="{{ route('Car.index') }}">Cars</a></div>
           <div class="lineMeun d-flex flex-row"><i class="bi bi-person-gear"></i><a href="{{ route('Driver.index') }}">Driver</a></div>
